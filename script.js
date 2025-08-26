470065840b28e6f32d9bf7e15325294d47642dcf
Nutan Arjun Shinde <shindenutan3714@gmail.com>
2025-08-26 20:01:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const navLinks = document.querySelector('.nav-links'); 
   
 mobileMenuBtn.addEventListener('click', () => {  
-    navLinks.classList.toggle('active');
+    navLinks.classList.toggle('active'); 
 }); 
 
 // Smooth Scrolling 
